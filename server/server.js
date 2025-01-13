75d2d89185b4e22f3ad3ff471b9bd527469502c9
sabbir-183023 <sabbir.us.183023@gmail.com>
2025-01-13 17:41:13 +0600

    do it

--- server/server.js
+++ server/server.js
@@ -7,16 +7,16 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoute.js";
 import cors from "cors";
-import path from "path";
+// import path from "path";
 import bodyParser from "body-parser";
-import { fileURLToPath } from "url";
+// import { fileURLToPath } from "url";
 
 // config env
 dotenv.config();
 
 //esmodule fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 //database config
 connectDB();
@@ -29,17 +29,18 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "./client/build")));
+// app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
+app.use("/", "lizas collection");
 
 //rest api
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.use("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 //PORT
 const PORT = process.env.PORT || 8080;
